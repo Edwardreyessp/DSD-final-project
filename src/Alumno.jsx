25036c2abf46a83d85dd8ed36e251ea868ce2b53
Edwardreyessp <edwardreyessp@gmail.com>
2024-01-01 18:49:38 -0600

    feat(Alumno):Dashboard y login

---
+++ src/Alumno.jsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+const Alumno = () => {
+  const dataStudent = {
+    name: "Edward Daniel Reyes Pérez",
+  };
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Materia 1", 9, 9, 9, 9),
+    createData("Materia 2", 9, 9, 9, 9),
+    createData("Materia 3", 9, 9, 9, 9),
+    createData("Materia 4", 9, 9, 9, 9),
+    createData("Materia 5", 9, 9, 9, 9),
+  ];
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <Box sx={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+        <img
+          src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
+          alt="alumno"
+          width={200}
+        />
+        <Typography variant="h1">{dataStudent.name}</Typography>
+      </Box>
+      <Button variant="contained">Cambiar foto de perfil</Button>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Materia</TableCell>
+              <TableCell align="right">Primer parcial</TableCell>
+              <TableCell align="right">Segundo parcial</TableCell>
+              <TableCell align="right">Tercer parcial</TableCell>
+              <TableCell align="right">Calificación final</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default Alumno;
--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+        flexDirection: "column",
+        gap: "1rem",
+      }}
+    >
+      <Typography variant="h1">Iniciar Sesión</Typography>
+      <TextField label="Usuario" />
+      <TextField label="Contraseña" />
+      <Button variant="contained">Iniciar Sesión</Button>
+    </Box>
   );
 }
 
